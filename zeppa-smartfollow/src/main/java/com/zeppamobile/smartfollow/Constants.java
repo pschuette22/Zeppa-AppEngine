5ebac29f2ea0e7d54ffbc9d453f094627743b26b
Pete Schuette <pschuette22@gmail.com>
2015-08-24 00:17:02 -0400

    Smartfollow setup

---
+++ zeppa-smartfollow/src/main/java/com/zeppamobile/smartfollow/Constants.java
@@ -0,0 +1,5 @@
+package com.zeppamobile.smartfollow;
+
+public class Constants {
+
+}
---
+++ zeppa-smartfollow/src/main/java/com/zeppamobile/smartfollow/Utils.java
@@ -0,0 +1,11 @@
+package com.zeppamobile.smartfollow;
+
+public class Utils {
+
+	/**
+	 * @Constructor private constructor as this is a Utility file
+	 */
+	private Utils() {
+	}
+
+}

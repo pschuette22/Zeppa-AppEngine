68ccf1330d708ddbcf8d6a6149d2ca756ab3a777
Pete Schuette <pschuette22@gmail.com>
2015-08-31 06:47:18 -0400

    Calm before the crazy shit storm

---
+++ zeppa-common/src/main/java/com/zeppamobile/common/App.java
@@ -0,0 +1,13 @@
+package com.zeppamobile.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

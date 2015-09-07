11e8aa567761f57a161ca290e099b96dc8a0bda5
Pete Schuette <pschuette22@gmail.com>
2015-09-07 13:04:52 -0400

    Added Static Test Config Value

--- zeppa-smartfollow/src/test/java/com/zeppamobile/smartfollow/agent/TestTagAgent.java
+++ zeppa-smartfollow/src/test/java/com/zeppamobile/smartfollow/agent/TestTagAgent.java
@@ -1,10 +1,16 @@
 package com.zeppamobile.smartfollow.agent;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.zeppamobile.common.datamodel.EventTag;
+import com.zeppamobile.common.utils.ModuleUtils;
 import com.zeppamobile.smartfollow.Configuration;
 
 public class TestTagAgent {
@@ -12,22 +18,37 @@ public class TestTagAgent {
 	private UserAgent userAgent;
 	private EventTag tag;
 	
-//	@Before
-//	private void setup(){
-//		Configuration.startTesting();
-//		userAgent = new UserAgent(Long.valueOf(1));
-//		tag = new EventTag(userAgent.getUserId(), "TestingPrograms");
-//	}
-//	
-//	@After
-//	public void teardown(){
-//		Configuration.stopTesting();
-//	}
-//	
-//	@Test
-//	private void doTesting(){
-////		TagAgent agent = new TagAgent(userAgent, tag);
-//	}
+	@Before
+	private void setup(){
+		Configuration.startTesting();
+		userAgent = new UserAgent(Long.valueOf(1));
+		tag = new EventTag(userAgent.getUserId(), "TestingPrograms");
+	}
 	
+	@After
+	public void teardown(){
+		Configuration.stopTesting();
+	}
+	
+	@Test
+	private void doTesting(){
+//		TagAgent agent = new TagAgent(userAgent, tag);
+	}
+	
+	
+	
+	private EventTag fetchTagById(Long tagId) throws MalformedURLException{
+		EventTag result = null;
+		
+		
+		Dictionary<String,String> params = new Hashtable<String,String>();
+		params.put("id",String.valueOf(tagId));
+		
+		URL url = ModuleUtils.getZeppaAPIUrl("getEventTag", params);
+		
+		
+		
+		return result;
+	}
 	
 }

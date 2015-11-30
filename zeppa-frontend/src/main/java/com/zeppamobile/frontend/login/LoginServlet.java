19b1e4fe1a01d1590b6a59e86b9170948b097eb0
Kieran Lynn <Kieran.j.lynn@gmail.com>
2015-11-30 15:40:46 -0500

    Fixed login servlet so it can handle access tokens and sets a universal cookie

--- zeppa-frontend/src/main/java/com/zeppamobile/frontend/login/LoginServlet.java
+++ zeppa-frontend/src/main/java/com/zeppamobile/frontend/login/LoginServlet.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,6 +14,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gwt.user.client.Cookies;
+import com.zeppamobile.common.UniversalConstants;
 import com.zeppamobile.common.utils.ModuleUtils;
 import com.zeppamobile.common.utils.Utils;
 
@@ -41,8 +44,10 @@ public class LoginServlet extends HttpServlet{
 		/*
 		 * Get the tag text from parameters
 		 */
-		String token = req.getParameter("token");
 
+		String token = req.getParameter("token");
+		resp.getWriter().println("token: " + token);
+		
 		if (Utils.isWebSafe(token)) {
 
 			/*
@@ -55,31 +60,8 @@ public class LoginServlet extends HttpServlet{
 			 * Read from the request
 			 */
 			try {
-//				resp.getWriter().println("Building Url: ");
-
-				URL url = ModuleUtils.getZeppaModuleUrl("zeppa-api",
-						"login", params);
-				
-//				resp.getWriter().print(url.toString() + "\n");
-
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(url.openStream()));
-				String line;
-
-				// Read from the buffer line by line and write to the response
-				// item
-				StringBuilder message = new StringBuilder();
-				
-				while ((line = reader.readLine()) != null) {
-					resp.getWriter().write(line+"\n");
-//					resp.getWriter().println(line);
-					message.append(line);
-					message.append("\n");
-				}
-
-				reader.close();
-
-				req.setAttribute("message", message.toString());
+				resp.getWriter().println("Building Url: ");
+				Cookies.setCookie(UniversalConstants.PARAM_ID_TOKEN,token);
 				
 				// Set there response status
 				response = "SC_OK";
@@ -105,10 +87,11 @@ public class LoginServlet extends HttpServlet{
 			 * If bad parameters were passed
 			 */
 			response = "SC_BAD_REQUEST";
+			resp.getWriter().println("Response: " + response);
 			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 		}
 		
-//		resp.getWriter().println("Response: " + response);
+		resp.getWriter().println("Response: " + response);
 
 	}
 
--- zeppa-frontend/src/main/webapp/login.jsp
+++ zeppa-frontend/src/main/webapp/login.jsp
@@ -7,27 +7,61 @@
 <meta name="google-signin-client_id" content="9207387879-274c5k1el52drcg0ou6l0josl5klbv8s.apps.googleusercontent.com">
 <title>Zeppa, Comare Tags</title>
 
-<script src="https://apis.google.com/js/platform.js" async defer></script>
-
+<script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
+<script src="js/jquery-2.1.4.min.js"></script>
 <script type="text/javascript">
-function onSignIn(googleUser) {
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+/*function onSignIn(googleUser) {
 	  var profile = googleUser.getBasicProfile();
+	  console.log(profile);
 	  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
 	  console.log('Name: ' + profile.getName());
 	  console.log('Image URL: ' + profile.getImageUrl());
 	  console.log('Email: ' + profile.getEmail());
-	}
+	}*/
+function onSuccess(googleUser) {
+	var profile = googleUser.getBasicProfile();
+    console.log(googleUser);
+    console.log(googleUser.po.access_token);
+    var access_token = googleUser.po.id_token;
+    var data = {'token': access_token}
+    $.post( "/login", data, function( resp ) {
+    	 	console.log("success");
+    	}).fail(function() {
+    	    console.log( "error" );
+    	});
+    /*
+    $.ajax({
+        url: '/login',
+        data: {
+            'token': access_token
+        },
+        type: 'POST'
+    }).done(function(){
+        alert("success");
+    }).fail(function(){
+        alert( "error" );
+    });​*/
+}
+
+  function onFailure(error) {
+    console.log(error);
+  }
+  function renderButton() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'https://www.googleapis.com/auth/userinfo.email',
+      'onsuccess': onSuccess,
+      'onfailure': onFailure
+    });
+  }
 </script>
 </head>
 <body>
 
-	<h1>Login</h1>
-
-	<form action="/compare-tags" method="post">
-	
-		<div class="g-signin2" data-onsuccess="onSignIn"></div>
-
-	</form>
+	<h1>Login</h1>		
+	<div id="my-signin2" class="g-signin2"></div>
 
 
 </body>

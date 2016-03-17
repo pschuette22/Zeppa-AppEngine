0746886a3fb2bb17cedc33b4e6af9debfe8c1882
Pete Schuette <pschuette22@gmail.com>
2016-03-17 11:47:55 -0400

    Added entities for meta tagging

--- zeppa-api/src/main/java/com/zeppamobile/api/datamodel/VendorEvent.java
+++ zeppa-api/src/main/java/com/zeppamobile/api/datamodel/VendorEvent.java
@@ -77,6 +77,7 @@ public class VendorEvent extends ZeppaEvent {
 	
 	@NotPersistent
 	private  VendorEventRelationship relationship;
+	
 
 	public String getPlaceId() {
 		return placeId;
@@ -97,107 +98,5 @@ public class VendorEvent extends ZeppaEvent {
 		this.relationship = relationship;
 	}
 	
-	
-	
 
-//	@PrimaryKey
-//	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
-//	private Key key;
-//
-//	@Persistent
-//	private Long vendorId;
-//	
-//	@Persistent
-//	private Long eventId;
-//	
-//	@Persistent
-//	private String title;
-//	
-//	@Persistent
-//	private String description;
-//	
-//	@Persistent
-//	private Long start;
-//	
-//	@Persistent
-//	private Long end;
-//	
-//	@Persistent
-//	private Long created;
-//	
-//	@Persistent
-//	List<EventTag> tags = new ArrayList<EventTag>();
-//	
-//	//	public Long getVendorId() {
-//return vendorId;
-//}
-//
-///**
-//* Convert this object to a json object
-//* 
-//* @return jsonObject
-//*/
-//@SuppressWarnings("unchecked")
-//public JSONObject toJson() {
-//JSONObject obj = new JSONObject();
-//
-//obj.put("key", key);
-//obj.put("created", created == null ? Long.valueOf(-1) : created);
-//obj.put("start", start == null ? Long.valueOf(-1) : start);
-//obj.put("end", start == null ? Long.valueOf(-1) : end);
-//obj.put("title", title);
-//obj.put("description", description);
-//obj.put("vendorId", vendorId);
-//
-//return obj;
-//}
-
-//	public Long getVendorId() {
-//		return vendorId;
-//	}
-//	public void setVendorId(Long vendorId) {
-//		this.vendorId = vendorId;
-//	}
-//	public Long getEventId() {
-//		return eventId;
-//	}
-//	public void setEventId(Long eventId) {
-//		this.eventId = eventId;
-//	}
-//	public String getTitle() {
-//		return title;
-//	}
-//	public void setTitle(String title) {
-//		this.title = title;
-//	}
-//	public String getDescription() {
-//		return description;
-//	}
-//	public void setDescription(String description) {
-//		this.description = description;
-//	}
-//	public Long getStart() {
-//		return start;
-//	}
-//	public void setStart(Long start) {
-//		this.start = start;
-//	}
-//	public Long getEnd() {
-//		return end;
-//	}
-//	public void setEnd(Long end) {
-//		this.end = end;
-//	}
-//	public Long getCreated() {
-//		return created;
-//	}
-//	public void setCreated(Long created) {
-//		this.created = created;
-//	}
-//	public List<EventTag> getTags() {
-//		return tags;
-//	}
-//	public void addTag(EventTag tag) {
-//		this.tags.add(tag);
-//	}
 }

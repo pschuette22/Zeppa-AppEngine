544d4df8a5f4e385e2d30465196ae721965b7754
Pete Schuette <pschuette22@gmail.com>
2016-03-31 14:25:00 -0400

    Privakey Client ID/secret

--- zeppa-api/src/main/java/com/zeppamobile/api/Constants.java
+++ zeppa-api/src/main/java/com/zeppamobile/api/Constants.java
@@ -57,5 +57,13 @@ public class Constants {
 	 */
 	public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
 	
+	
+	/*
+	 * Privakey 
+	 */
+	public static final String PRIVAKEY_CLIENT_ID = "047b5731c6fe4fc79d1eda8dae46e4d2";
+	public static final String PRIVAKEY_CLIENT_SECRET = "clglJQEdtMw2794KI53e0q9SYHJ5_pCCy1sZOF-P0";
 
+	
+	
 }

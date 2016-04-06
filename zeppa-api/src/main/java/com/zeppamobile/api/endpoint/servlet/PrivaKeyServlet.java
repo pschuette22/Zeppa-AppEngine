136abba7836aa9a1b704e65bc77fa261d8ca23f7
Brendan Kennedy <bken123@gmail.com>
2016-04-05 20:59:49 -0400

    Added base64 decoding of claims values which are received from the PrivaKey authentication service

--- zeppa-api/src/main/java/com/zeppamobile/api/endpoint/servlet/PrivaKeyServlet.java
+++ zeppa-api/src/main/java/com/zeppamobile/api/endpoint/servlet/PrivaKeyServlet.java
@@ -1,9 +1,12 @@
 package com.zeppamobile.api.endpoint.servlet;
 
+import java.awt.Desktop;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
@@ -17,6 +20,9 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import com.google.api.client.repackaged.org.apache.commons.codec.binary.Base64;
+import com.google.appengine.repackaged.org.apache.commons.codec.binary.StringUtils;
+import com.google.gson.JsonElement;
 import com.zeppamobile.api.Constants;
 
 /**
@@ -52,18 +58,13 @@ public class PrivaKeyServlet extends HttpServlet {
 		s += "&scope=openid";
 		s += "&redirect_uri=" + URLEncoder.encode("https://1-dot-zeppa-api-dot-zeppa-cloud-1821.appspot.com/privakey/", "UTF-8");
 		s += "&nonce=" + URLEncoder.encode(email, "UTF-8");
-		//s += "&login_hint=openid";
-		URL url = new URL(s);
+		s += "&login_hint=" + URLEncoder.encode(email, "UTF-8");
+		URI url = new URI(s);
 		
 		response.getWriter().append("PrivaKey URL: " + s);
 		
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setDoOutput(false);
-        connection.setRequestMethod("GET");
-        
-        connection.connect();
-        
-        response.getWriter().append("Tesing after connect ");
+		Desktop.getDesktop().browse(url); //PrivaKey requires the URL to be open in a new browser
+		
         
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -79,14 +80,24 @@ public class PrivaKeyServlet extends HttpServlet {
 		String token = request.getParameter("id_token");
 		
 		response.getWriter().append("PrivaKey ID Token: " + token);
+		
+		String[] base64EncodedSegments = token.split("\\.");
+		 
+
 		try {
-			JSONObject json = (JSONObject)new JSONParser().parse(token);
+			String base64EncodedHeader = base64EncodedSegments[0];
+			String base64EncodedClaims = base64EncodedSegments[1];
+			JSONParser parser = new JSONParser();
+			JsonElement payload = (JsonElement) parser.parse(StringUtils.newStringUtf8(Base64.decodeBase64(base64EncodedClaims)));
+			JSONObject claims = (JSONObject)new JSONParser().parse(payload.toString());
 			
-			String sub = (String) json.get("sub");
-			String email = URLDecoder.decode((String) json.get("nonce"), "UTF-8");// This value should be checked to match the one sent in the request
-			String iss = (String) json.get("iss"); // Should be https://idp.privakeyapp.com/identityserver
-			String aud = (String) json.get("aud"); //Should contain our client id
-			String expTime = (String) json.get("exp");
+			response.getWriter().append("PrivaKey Claims:" + claims);
+			
+			String sub = (String) claims.get("sub");
+			String email = (String) claims.get("nonce");// This value should be checked to match the one sent in the request
+			String iss = (String) claims.get("iss"); // Should be https://idp.privakeyapp.com/identityserver
+			String aud = (String) claims.get("aud"); //Should contain our client id
+			String expTime = (String) claims.get("exp");
 			response.getWriter().append("PrivaKey ID Sub Value:" + sub);
 			
 			EmployeeServlet.updateEmployeePrivaKeyID(email, sub);

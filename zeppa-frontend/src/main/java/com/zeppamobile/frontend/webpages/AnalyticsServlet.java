13e81cee1311037ab24e48df273cb7b778eeea7e
Kevin <kem345@drexel.edu>
2016-04-20 12:45:44 -0400

    Removing print statements and formating analytics

--- zeppa-frontend/src/main/java/com/zeppamobile/frontend/webpages/AnalyticsServlet.java
+++ zeppa-frontend/src/main/java/com/zeppamobile/frontend/webpages/AnalyticsServlet.java
@@ -56,8 +56,6 @@ public class AnalyticsServlet extends HttpServlet {
 		minAgeFilter = req.getParameter("minAge");
 		maxAgeFilter = req.getParameter("maxAge");
 		genderFilter = req.getParameter("gender");
-//		System.out.println("----start: "+startDateFilter);
-//		System.out.println("----end: "+endDateFilter);
 		
 		HttpSession session = req.getSession(true);
 		Object obj = session.getAttribute("UserInfo");
